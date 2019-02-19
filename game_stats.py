8bb1185d3c8feb4fa3849ccbc206d13dd30d5ea8
Marcos <36970051+LordUllr@users.noreply.github.com>
2019-02-19 10:36:08 -0300

    Status do Jogo
    Incrementado a função de salvar a pontuação máxima em um arquivo .txt

--- game_stats.py
+++ game_stats.py
@@ -1,15 +1,20 @@
+import json
+
 class GameStats():
     """Armazena dados estatisticos do Jogo"""
-
 
     def __init__(self, ai_settings):
         """Inicializa os dados estatisticos"""
         self.ai_settings = ai_settings
         self.reset_stats()
-        #Inicia a Invasão Alien em um estado ativo
+        #Inicia a Invasão Alien em um estado inativo
         self.game_active = False
+        self.get_high_score = 0
         #Pontuação máxima conquistada pelo jogador
-        self.high_score = 0
+        with open('high_score.txt') as f_obj:
+            for line in f_obj.readlines():
+                self.get_high_score = int(line)
+        self.high_score = self.get_high_score
 
     def reset_stats(self):
         """Inicializa os dados que podem mudar durante o jogo"""
